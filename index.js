26582e11281e2edc37a843ebba753585e311ca06
VoidGeek <pradyumna.p.6969@gmail.com>
2023-11-03 22:43:22 +0530

    Initial

--- index.js
+++ index.js
@@ -1,14 +1,125 @@
-// Import packages
+const MONGODB_URI = "mongodb+srv://pradyumnap6969:4cgMcwdSO2JeO0kI@test.fu9uije.mongodb.net/KKK";
+const COOKIE_SECRET = "your_cookie_secret_key";
+
 const express = require("express");
-const home = require("./routes/home");
+const cors = require("cors");
+const cookieSession = require("cookie-session");
+const mongoose = require("mongoose");
 
-// Middlewares
 const app = express();
+
+app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// Routes
-app.use("/home", home);
+app.use(
+  cookieSession({
+    name: "bezkoder-session",
+    keys: [COOKIE_SECRET],
+    httpOnly: true,
+  }));
 
-// connection
-const port = process.env.PORT || 9001;
-app.listen(port, () => console.log(`Listening to port ${port}`));
+// Set up the MongoDB connection
+try {
+  mongoose
+    .connect(MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("Successfully connect to MongoDB.");
+    })
+    .catch((err) => {
+      console.error("Connection error", err);
+      process.exit(1); // Exit the process with an error code
+    });
+} catch (error) {
+  console.error("Error connecting to MongoDB:", error);
+  process.exit(1); // Exit the process with an error code
+}
+
+app.get("/", (req, res) => {
+  try {
+    res.json({ message: "Welcome to bezkoder application." });
+  } catch (error) {
+    console.error("Error handling the / route:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+// Import the contact and project routers
+// const contactRouter = require("./app/routes/contact.routes");
+// const projectRouter = require("./app/routes/project.routes"); // Import the project router
+// const postRoutes = require("./app/routes/postRoutes");
+// const serviceRouter = require("./app/routes/service.routes")
+// const testimonialRouter = require("./app/routes/testimonial.routes")
+// const imageRoutes = require('./app/routes/imageRoutes')
+// const resetRouter=require('./app/routes/reset.routes')
+
+// app.use("/", contactRouter); // Mount the contact router on the /api/contacts route
+// app.use("/", projectRouter); // Mount the project router on the /api/projects route
+
+// app.use("/", serviceRouter);
+// app.use("/", testimonialRouter);
+// app.use('/', postRoutes);
+// app.use('/api', imageRoutes);
+// app.use('/api',resetRouter);
+
+// Import the authentication and user routes (replace with actual paths)
+// try {
+//   require("./app/routes/auth.routes")(app);
+//   require("./app/routes/user.routes")(app);
+// } catch (error) {
+//   console.error("Error setting up routes:", error);
+// }
+
+const PORT = 8080;
+try {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+  });
+} catch (error) {
+  console.error("Error starting the server:", error);
+}
+
+// Set up the initial roles
+// function initial() {
+//   try {
+//     const Role = require("./app/models/role.model");
+
+//     Role.estimatedDocumentCount((err, count) => {
+//       if (!err && count === 0) {
+//         new Role({
+//           name: "user",
+//         }).save((err) => {
+//           if (err) {
+//             console.log("error", err);
+//           }
+
+//           console.log("added 'user' to roles collection");
+//         });
+
+//         new Role({
+//           name: "admin",
+//         }).save((err) => {
+//           if (err) {
+//             console.log("error", err);
+//           }
+
+//           console.log("added 'admin' to roles collection");
+//         });
+//         new Role({
+//           name: "moderator"
+//         }).save(err => {
+//           if (err) {
+//             console.log("error", err);
+//           }
+  
+//           console.log("added 'moderator' to roles collection");
+//         });
+//       }
+//     });
+//   } catch (error) {
+//     console.error("Error in the initial setup:", error);
+//   }
+// }

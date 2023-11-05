f664c8e19d71d072a18e356dcb9ac0bb70a8575d
VoidGeek <pradyumna.p.6969@gmail.com>
2023-11-05 11:21:03 +0530

    Update

--- routes/project.routes.js
+++ routes/project.routes.js
@@ -9,17 +9,17 @@ const multer = require('multer');
 // Create a new project (POST)
 
 
-router.post("/api/projects",[authJwt.verifyToken], projectController.createProject,);
+router.post("/api/projects", [authJwt.verifyToken, authJwt.isAdmin], projectController.createProject,);
 
 // Get all projects (GET)
 router.get("/api/projects", projectController.getAllProjects);
 
 // Get a specific project by ID (GET)
+router.get("/api/projects/:id", projectController.getProjectById);
 
-router.get("/api/projects/:id", projectController.getAllProjects);
 // Update a project by ID (PUT)
-router.put("/api/projects/:id", projectController.updateProject);
+router.put("/api/projects/:id", [authJwt.verifyToken, authJwt.isAdmin,checkAdminPermission], projectController.updateProject);
 
 // Delete a project by ID (DELETE)
-router.delete("/api/projects/:id",  projectController.deleteProject);
+router.delete("/api/projects/:id", [authJwt.verifyToken, authJwt.isAdmin,checkAdminPermission], projectController.deleteProject);
 module.exports = router;
